14d16d785eaecadbdb224b1a35b6ae4f85cb6fb3
Yuya Nozaki <Yuya.Nozaki@ibm.com>
2023-02-19 19:03:34 +0900

    MemberNFT:テストの前処理実装

--- test/MemberNFT.js
+++ test/MemberNFT.js
@@ -2,23 +2,25 @@ const { expect } = require("Chai");
 const {ethers} = require("hardhat");
 
 describe("MemberNFTコントラクト", () => {
-  it("トークンの名前とシンボルがセットされるべき", async () => {
-    const name = "MemberNFT";
-    const symbol = "MEM";
-    const MemberNFT = await ethers.getContractFactory("MemberNFT");
-    const memberNFT = await MemberNFT.deploy();
-    await memberNFT.deployed();
+  let MemberNFT;
+  let memberNFT;
+  const name = "MemberNFT";
+  const symbol = "MEM";
+  let owner;
 
+  beforeEach(async () =>{
+    [owner] = await ethers.getSigners();
+    MemberNFT = await ethers.getContractFactory("MemberNFT");
+    memberNFT = await MemberNFT.deploy();
+    await memberNFT.deployed();
+  });
+
+  it("トークンの名前とシンボルがセットされるべき", async () => {
     expect(await memberNFT.name()).to.equal(name);
     expect(await memberNFT.symbol()).to.equal(symbol);
 
   });
   it("デプロイアドレスがownerに設定されるべき", async () => {
-    const [owner] = await ethers.getSigners();
-    const MemberNFT = await ethers.getContractFactory("MemberNFT");
-    const memberNFT = await MemberNFT.deploy();
-    await memberNFT.deployed();
-
     expect(await memberNFT.owner()).to.equal(owner.address);
   });
 });
